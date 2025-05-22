1bacc6915d53cf9d5e69e9ea9f32e0b8ba29d343
KINGINTHECASTE123 <jacbisgaard@hotmail.com>
2025-05-22 20:24:06 +0200

    Intro screen 3 done with correct architecture

--- app/src/main/java/com/example/klimaaktion/view/introscreen3/IntroScreen3.kt
+++ app/src/main/java/com/example/klimaaktion/view/introscreen3/IntroScreen3.kt
@@ -1,80 +1,11 @@
 package com.example.klimaaktion.view.introscreen3
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.compose.foundation.Image
-import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavController
-import com.example.klimaaktion.R
+import com.example.klimaaktion.view.introscreen3.components.IntroScreen3Content
 
-// kode skrevet af Jacob
+// Skrevet af Jacob
 @Composable
 fun IntroScreen3(navController: NavController) {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color(0xFF94D7F2))
-            .padding(24.dp)
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .align(Alignment.TopCenter),
-            horizontalAlignment = Alignment.Start
-        ) {
-            Spacer(modifier = Modifier.height(100.dp))
-
-            Text(
-                text = "Følg",
-                fontSize = 32.sp,
-                fontWeight = FontWeight.ExtraBold,
-                color = Color(0xFF202020)
-            )
-
-            Spacer(Modifier.height(4.dp))
-
-            Text(
-                text = "Se din fremgang og optjen trofæer!",
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Medium,
-                color = Color(0xFF202020)
-            )
-
-            Spacer(modifier = Modifier.height(40.dp))
-
-            Image(
-                painter = painterResource(id = R.drawable.intro_card),
-                contentDescription = "Progress box",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(260.dp)
-                    .align(Alignment.CenterHorizontally)
-            )
-
-            Spacer(modifier = Modifier.height(24.dp))
-
-            Image(
-                painter = painterResource(id = R.drawable.trophy),
-                contentDescription = "Trophies box",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(180.dp)
-                    .align(Alignment.CenterHorizontally)
-            )
-        }
-
-        BottomSection3(
-            modifier = Modifier
-                .align(Alignment.BottomCenter)
-                .padding(bottom = 24.dp)
-        )
-    }
+    IntroScreen3Content(navController = navController)
 }
