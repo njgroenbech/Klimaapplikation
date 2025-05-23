5a70e528ea2e8baa38f88f6bc91dc0d32c17485c
Munkeflunke <laha0011@stud.kea.dk>
2025-05-23 13:58:13 +0200

    ADDED MORE PARAM NAVCONTROLLER PICTURE ON STARTSCREEN CHANGED  ROUTES

--- app/src/main/java/com/example/klimaaktion/view/taskscreen/TaskScreen.kt
+++ app/src/main/java/com/example/klimaaktion/view/taskscreen/TaskScreen.kt
@@ -1,4 +1,4 @@
-package com.example.klimaaktion.view.taskScreen
+package com.example.klimaaktion.view.taskscreen
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
