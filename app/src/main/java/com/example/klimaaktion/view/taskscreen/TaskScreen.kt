1f0c8010a68356630ddffd7ea02c03727a5ae8de
user4230 <nicholasgladik@hotmail.com>
2025-05-23 14:17:51 +0200

    progress screen

--- app/src/main/java/com/example/klimaaktion/view/taskscreen/TaskScreen.kt
+++ app/src/main/java/com/example/klimaaktion/view/taskscreen/TaskScreen.kt
@@ -1,13 +1,13 @@
-package com.example.klimaaktion.view.taskscreen
+package com.example.klimaaktion.view.taskScreen
 
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
