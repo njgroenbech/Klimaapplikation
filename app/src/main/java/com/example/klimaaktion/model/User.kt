bd5a53460e2fc1c4902ce05fdd099f8f09863618
eliasbwgarcia <eliasbenjamin@live.dk>
2025-05-29 18:32:17 +0200

    Elias: Kommenteret min kode og opdateret navne på leaderboardet.

--- app/src/main/java/com/example/klimaaktion/model/User.kt
+++
@@ -1,9 +0,0 @@
-package com.example.klimaaktion.model
-
-// Kode skrevet af Elias
-// Klassen bruges til databasen i firebase.
-
-data class User(
-    val username: String = "",
-    val email: String = ""
-)
